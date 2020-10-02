ccce9b8f9e6f84b9e204ef780e243ab659cfbeb8
Emmanuel <tabar.emmanuel@gmail.com>
2020-10-02 12:44:55 +0800

    Updates

---
+++ precache-manifest.0453b5e860879354f019a45622b8a847.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f49e246fbd4fb70439bb73ce37b86041",
+    "url": "/oct2020/index.html"
+  },
+  {
+    "revision": "d276f6b0ba490f938f25",
+    "url": "/oct2020/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2e2d3fd71fd8c9aefae3",
+    "url": "/oct2020/static/js/2.f8fc3ef9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/oct2020/static/js/2.f8fc3ef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d276f6b0ba490f938f25",
+    "url": "/oct2020/static/js/main.dd2d34a6.chunk.js"
+  },
+  {
+    "revision": "3d42586a805da4af7812",
+    "url": "/oct2020/static/js/runtime-main.c97f6103.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/oct2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
